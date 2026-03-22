994caf0add866bf968dcbd14118ba3a3a16d0db0
Ritesh Mishra <riteshmishra9565@gmail.com>
2026-03-22 09:18:44 +0530

    voter model created for testing

--- src/models/Voter.model.js
+++ src/models/Voter.model.js
@@ -1,16 +1,24 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const voterSchema = new mongoose.Schema({
 
     event: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Event",
-        required: true
+        required: [true, "Event is required"]
     },
 
-    voterID: {
+    voterIDHash: {
         type: String,
-        required: true
+        required: [true, "Voter is required"]
+    },
+
+    // Candidate
+    candidate: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Candidate",
+        required: [true, "Candidate is required"]
     },
 
     votedAt: {
@@ -19,5 +27,23 @@ const voterSchema = new mongoose.Schema({
     }
 
 }, { timestamps: true });
+
+
+// store hash for privacy
+voterSchema.pre("save", async function (next) {
+
+    // already hashed hai to skip karo
+    if (!this.isModified("voterIDHash")) {
+        return next();
+    }
+
+    // voter ko private krna 
+    const hash = await bcrypt.hash(this.voterIDHash, 10);
+
+    // save
+    this.voterIDHash = hash;
+
+    next();
+});
 
 module.exports = mongoose.model("Voter", voterSchema);
